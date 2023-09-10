03d8f5f7c26d9ee9b779cf9f9d7a9bcc4dd2ca48
JOEL OTHINIEL SOMAKPO <othinieljo@gmail.com>
2023-09-10 20:27:08 +0000

    Commit ....

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("share-button").addEventListener("click", function() {
     var linkToShare = "https://othinieljo.github.io/Test/";
 
     // Utilisez l'API WhatsApp pour ouvrir WhatsApp avec le message et le lien
-    var message = "Car Dieu a tant aimé le monde qu'il a donné son Fils unique, afin que quiconque croit en lui ne périsse point, mais qu'il ait la vie éternelle <a href='" + linkToShare + "'>message de vie</a>";
+    var message = "Car Dieu a tant aimé le monde qu'il a donné son Fils unique, afin que quiconque croit en lui ne périsse point, mais qu'il ait la vie éternelle [message de vie](" + linkToShare + ")";
     var whatsappURL = "whatsapp://send?text=" + encodeURIComponent(message);
 
     // Redirigez l'utilisateur vers WhatsApp
